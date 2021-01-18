be09eefe766cc70db9a9b548a96121d38d49c5e2
nobodyzxc <nobodyzxc.tw@gmail.com>
2021-01-18 12:06:30 +0800

    StateChange to LoadEnd

--- App.js
+++ App.js
@@ -5,62 +5,46 @@ import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   webview = null;
+  chatloc = "lounge"
 
   render() {
     return (
       <WebView
         ref={(ref) => (this.webview = ref)}
         source={{ uri: 'https://drrr.com' }}
+        originWhitelist={['https://drrr.com', 'https://drrr.com/*']}
         style={{ marginTop: 20 }}
-        onNavigationStateChange={this.handleWebViewNavigationStateChange}
+        onLoadEnd={this.handleLoadEnd}
+        onMessage={this.handleMessage}
       />
     );
   }
 
-  handleWebViewNavigationStateChange = (newNavState) => {
-    // newNavState looks something like this:
-    // {
-    //   url?: string;
-    //   title?: string;
-    //   loading?: boolean;
-    //   canGoBack?: boolean;
-    //   canGoForward?: boolean;
-    // }
-    const { url } = newNavState;
-    if (!url) return;
-
-    // handle certain doctypes
-    if (url.includes('lounge')) {
-      // lounge
-      //this.webview.stopLoading();
-      this.webview.injectJavaScript('alert("welcome to lounge")');
+  handleLoadEnd = (syntheticEvent) => {
+    const { nativeEvent } = syntheticEvent;
+    if (nativeEvent.url.includes('lounge')) {
+      this.chatloc = "lounge";
+      this.webview.injectJavaScript(`
+        alert("L ${nativeEvent.url}");
+        true;
+        `);
     }
 
-    // one way to handle a successful form submit is via query strings
-    if (url.includes('room')) {
-      this.webview.injectJavaScript('alert("welcome to room")');
-      // room
-      //this.webview.stopLoading();
+    if (nativeEvent.url.includes('room')) {
+      this.chatloc = "room";
+      this.webview.injectJavaScript(`
+        alert("R ${nativeEvent.url}");
+        true;
+      `);
+      // window.ReactNativeWebView.postMessage
     }
+  };
 
-    // redirect somewhere else
-    //if (url.includes('google.com')) {
-    //  const newURL = 'https://reactnative.dev/';
-    //  const redirectTo = 'window.location = "' + newURL + '"';
-    //  this.webview.injectJavaScript(redirectTo);
-    //}
+  handleMessage = (event) => {
+    alert(event.nativeEvent.data);
   };
 
 }
-
-//export default function App() {
-//  return (
-//    <View style={styles.container}>
-//      <Text>Welcome to DuRaRaRa App</Text>
-//      <StatusBar style="auto" />
-//    </View>
-//  );
-//}
 
 const styles = StyleSheet.create({
   container: {
