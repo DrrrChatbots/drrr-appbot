d0646584894ffab2ec5ce852ea0c438286502e15
nobodyzxc <nobodyzxc.tw@gmail.com>
2021-01-19 04:08:31 +0800

    comm done

--- App.js
+++ App.js
@@ -43,6 +43,115 @@ const NavigationDrawerStructure = (props) => {
   );
 };
 
+var roomscript = `
+try{
+  function handle_talks(msg){
+    var event = {
+      type: undefined,
+      from: undefined,
+      text: undefined,
+      info: undefined,
+      url: undefined,
+      to: undefined
+    };
+    parser = {
+      "me": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).contents().filter(function() {
+          return this.nodeType == 3;
+        }).get().pop().textContent;
+        return event;
+      },
+      "music": function(msg, event){
+        names = $(msg).find('.name');
+        event.from = names[0].textContent;
+        event.text = names[1].textContent;
+        return event;
+      },
+      "leave": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).text();
+        return event;
+      },
+      "join": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).text();
+        return event;
+      },
+      "new-host": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).text();
+        return event;
+      },
+      "new-description": function(msg, event){
+        //event.from = $(msg).find('.name').text();
+        event.text = $(msg)[0].childNodes[3].textContent
+        return event;
+      },
+      "room-profile": function(msg, event){
+        //event.from = $(msg).find('.name').text();
+        event.text = $('.room-title-name').text()
+        return event;
+      },
+      "secret": function(msg, event){
+        dual = $(msg).find('.name span');
+        if(dual.length > 1){
+          event.to = dual[2].textContent;
+          event.from = dual[0].textContent;
+        } else event.from = $(msg).find('.name span').text();
+        event.text = $(msg).find($('.bubble p'))
+          .clone().children().remove().end().text();
+        return event;
+      },
+      "kick": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).text();
+        return event;
+      },
+      "ban": function(msg, event){
+        event.from = $(msg).find('.name').text();
+        event.text = $(msg).text();
+        return event;
+      }
+    }
+
+    wildcard = function(msg, event) {
+      event.type = "talk"
+      names = $(msg).find('.name');
+      if(names.length){
+        event.from = names[0].textContent;
+        //event.text = $(msg).find('.select-text')[1].textContent
+        event.text = $($(msg).find('.select-text')[1]).contents().filter(function() {
+          return this.nodeType == Node.TEXT_NODE;
+        }).text();
+      }
+      return event;
+    }
+
+    for(type in parser){
+      if(msg.classList.contains(type)){
+        event.type = type;
+        event = parser[type](msg, event);
+      }
+    }
+
+    if(!event.type) event = wildcard(msg, event);
+
+    var ue = $(msg).find($('.bubble p a'));
+    if(ue && ue.length) event.url = ue.attr('href');
+    ue = $(msg).find($('img'));
+    if(ue && ue.length) event.url = ue.attr('data-src');
+
+    return event;
+  }
+}
+catch (err) {
+  alert(JSON.stringify(err));
+}
+window.ReactNativeWebView.postMessage(JSON.stringify(handle_talks($('#talks')[0])));
+true;
+`;
+
 function firstScreenStack({navigation, route}) {
   return (
     <Stack.Navigator initialRouteName="FirstPage">
@@ -106,6 +215,10 @@ class Mobot {
   constructor(){
     this.webview = null;
     this.chatloc = null;
+    this.setWebview = this.setWebview.bind(this);
+    this.setChatloc = this.setChatloc.bind(this);
+    this.handleLoadEnd = this.handleLoadEnd.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
   }
 
   setWebview(ref){
@@ -114,31 +227,30 @@ class Mobot {
 
   setChatloc(loc){
     this.chatloc = loc;
-    alert(loc)
-  }
-
-  handleMessage(data){
-    alert(data);
-    // do something
+    //alert(loc)
   }
 
   handleLoadEnd = (syntheticEvent) => {
     const { nativeEvent } = syntheticEvent;
     if (nativeEvent.url.includes('lounge')) {
       this.setChatloc("lounge");
-      //this.mobot.webview.injectJavaScript(`
-      //  alert("L ${nativeEvent.url}");
-      //  true;
-      //  `);
+      this.webview.injectJavaScript(`
+        //alert("L ${nativeEvent.url}");
+        window.ReactNativeWebView.postMessage(JSON.stringify(profile))
+        true;
+        `);
     }
 
     if (nativeEvent.url.includes('room')) {
       this.setChatloc("room");
-      //this.mobot.webview.injectJavaScript(`
-      //  alert("R ${nativeEvent.url}");
+      this.webview.injectJavaScript(roomscript);
+      //this.webview.injectJavaScript(`
+      //  //alert("R ${nativeEvent.url}");
+      //  alert("done");
+      //  //window.ReactNativeWebView.postMessage(JSON.stringify(profile))
+      //  //window.ReactNativeWebView.postMessage(JSON.stringify(room.talks[0].id))
       //  true;
       //`);
-      // window.ReactNativeWebView.postMessage
     }
   };
 
